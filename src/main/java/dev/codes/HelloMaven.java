4ea596ebda2eae5e322f22332d57a63f75e7cf5c
kayan <yipkayan3@gmail.com>
2022-07-05 22:31:58 +0800

    Initial maven project + logging

---
+++ src/main/java/dev/codes/HelloMaven.java
@@ -0,0 +1,14 @@
+package dev.codes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMaven {
+
+    private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
+    public static void main(String[] args){
+        log.info("Hello Info");
+        log.debug("Hello debug");
+    }
+}
